cffd3abd20c3e9c71b9a4f64d9b46c22ae5f3a06
kobi_2016 <254633397@qq.com>
2016-05-13 10:55:19 +0800

    tt

---
+++ KBOpengl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KBOpengl
+//
+//  Created by chengshenggen on 5/13/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
