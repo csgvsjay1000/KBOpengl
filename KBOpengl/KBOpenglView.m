d126239058d8f94cf061bc85892d6c4b14d6aa2a
kobi_2016 <254633397@qq.com>
2016-05-15 16:11:26 +0800

    tt

--- KBOpengl/KBOpenglView.m
+++ KBOpengl/KBOpenglView.m
@@ -7,23 +7,117 @@
 //
 
 #import "KBOpenglView.h"
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+@interface KBOpenglView ()
+
+@property(nonatomic,strong)EAGLContext *context;
+
+@property(nonatomic,assign)GLuint framebuffer;
+@property(nonatomic,assign)GLuint renderbuffer;
+
+@end
 
 @implementation KBOpenglView
 
+#pragma mark - life cycle
 -(instancetype)init{
     self = [super init];
     if (self) {
         self.backgroundColor = [UIColor redColor];
+        [self doInit];
     }
     return self;
 }
 
-//-(id)initWithFrame:(CGRect)frame{
-//    self = [super initWithFrame:frame];
-//    if (self) {
-//        
-//    }
-//    return self;
-//}
+-(void)dealloc{
+    NSLog(@"%@ dealloc",[self class]);
+}
+
++(Class)layerClass{
+    return [CAEAGLLayer class];
+}
+
+-(void)doInit{
+    CAEAGLLayer *eaglLayer = (CAEAGLLayer*) self.layer;
+    eaglLayer.opaque = TRUE;
+    eaglLayer.drawableProperties = @{ kEAGLDrawablePropertyRetainedBacking :[NSNumber numberWithBool:NO],kEAGLDrawablePropertyColorFormat : kEAGLColorFormatRGBA8};
+    _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:_context];
+    
+}
+
+-(void)layoutSubviews{
+    [self createFrameAndRenderBuffer];
+}
+
+-(void)createFrameAndRenderBuffer{
+    
+    [self destoryFrameAndRenderBuffer];
+    
+    glGenFramebuffers(1, &_framebuffer);
+    glGenRenderbuffers(1, &_renderbuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
+    if (![_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer])
+    {
+        NSLog(@"attach渲染缓冲区失败");
+    }
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _renderbuffer);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        NSLog(@"创建缓冲区错误 0x%x", glCheckFramebufferStatus(GL_FRAMEBUFFER));
+    }
+}
+
+-(void)destoryFrameAndRenderBuffer{
+    if (_framebuffer) {
+        glDeleteFramebuffers(1, &_framebuffer);
+    }
+    if (_renderbuffer) {
+        glDeleteRenderbuffers(1, &_renderbuffer);
+    }
+    _framebuffer = 0;
+    _renderbuffer = 0;
+}
+
+#pragma mark - public methods
+-(void)render{
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    
+    
+}
+
+#pragma mark - private methods
+-(BOOL)loadShader{
+    
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *vertexShaderSource = "";
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("%s\n",infoLog);
+    }
+    
+    
+    return YES;
+}
 
 @end
